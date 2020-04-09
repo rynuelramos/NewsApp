f40080f7b55fddc456bde5d89829ddde9cba74dd
rynuelramos <ry.ramos1996@gmail.com>
2020-04-09 19:36:38 +0800

    first commit

---
+++ build/precache-manifest.a110b2e9f290c53200bb937a8ed711a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2eee624cc62eb2d386488a10048a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a4254d5be7e5417bcf7",
+    "url": "/static/css/main.d25cce07.chunk.css"
+  },
+  {
+    "revision": "e6efa711758858799b10",
+    "url": "/static/js/2.d88853d3.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.d88853d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4254d5be7e5417bcf7",
+    "url": "/static/js/main.033e1469.chunk.js"
+  },
+  {
+    "revision": "4899fb77a51acd5e823e",
+    "url": "/static/js/runtime-main.db082f84.js"
+  }
+]);
